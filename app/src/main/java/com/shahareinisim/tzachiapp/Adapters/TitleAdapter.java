6e4193c405de97b1dbfd676ef599f05a76362671
Jacob <jacobel640@gmail.com>
2023-07-31 04:05:55 +0300

    spelling and reduce notes

--- app/src/main/java/com/shahareinisim/tzachiapp/Adapters/TitleAdapter.java
+++ app/src/main/java/com/shahareinisim/tzachiapp/Adapters/TitleAdapter.java
@@ -2,7 +2,6 @@ package com.shahareinisim.tzachiapp.Adapters;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
